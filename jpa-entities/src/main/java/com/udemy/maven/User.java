f531a84a72c9c0bec0df87b9a7d988b1ee5d1400
Timur Celikel <tcelikel@gmail.com>
2023-08-27 10:07:54 -0700

    initial multi-module commit

---
+++ jpa-entities/src/main/java/com/udemy/maven/User.java
@@ -0,0 +1,22 @@
+package com.udemy.maven;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Data
+@Entity
+public class User {
+
+	@Id
+	private Long id;
+
+	private String firstName;
+	private String lastName;
+	private String email;
+}
